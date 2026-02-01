1bea2883afb62509fbfbfe11498a40bfd14f4381
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-02-01 19:04:24 +0530

    Anim Instance Variables Created

--- Source/UE5_DMC_Clone/Public/PlayerCharacter/DMC_PlayerAnimInstance.h
+++ Source/UE5_DMC_Clone/Public/PlayerCharacter/DMC_PlayerAnimInstance.h
@@ -4,8 +4,30 @@
 #include "Animation/AnimInstance.h"
 #include "DMC_PlayerAnimInstance.generated.h"
 
+class UCharacterMovementComponent;
+class ADMC_PlayerCharacter;
+
 UCLASS()
 class UE5_DMC_CLONE_API UDMC_PlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	// ~ Begin UAnimInstance interface
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	// ~ End UAnimInstance interface
+	
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Character")
+	TObjectPtr<ADMC_PlayerCharacter> PlayerCharacter;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Character")
+	TObjectPtr<UCharacterMovementComponent> PlayerCharacterMovement;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float Speed;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
 };
