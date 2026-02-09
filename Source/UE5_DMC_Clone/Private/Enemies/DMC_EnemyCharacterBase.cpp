511b5db5607483d92864289412300fb84a24cd54
Sayan Nandi <91082413+Sayan11071998@users.noreply.github.com>
2026-02-09 22:47:33 +0530

    Enemy Code cleaned-up

--- Source/UE5_DMC_Clone/Private/Enemies/DMC_EnemyCharacterBase.cpp
+++ Source/UE5_DMC_Clone/Private/Enemies/DMC_EnemyCharacterBase.cpp
@@ -12,26 +12,7 @@ void ADMC_EnemyCharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	if (bIsBuffering && BufferCurve)
-	{
-		BufferTimeElapsed += DeltaTime;
-		
-		if (BufferTimeElapsed <= BufferDuration)
-		{
-			float Alpha = BufferCurve->GetFloatValue(BufferTimeElapsed);
-			
-			FVector CurrentLoc = GetActorLocation();
-			FVector Forward = GetActorForwardVector();
-			FVector TargetLoc = CurrentLoc + (Forward * CurrentBufferAmount);
-			FVector NewLoc = FMath::Lerp(CurrentLoc, TargetLoc, Alpha * DeltaTime * 60.f);
-			
-			SetActorLocation(NewLoc, true);
-		}
-		else
-		{
-			StopBuffer();
-		}
-	}
+	UpdateBufferLogic(DeltaTime);
 }
 
 float ADMC_EnemyCharacterBase::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent,
@@ -59,35 +40,41 @@ float ADMC_EnemyCharacterBase::TakeDamage(float DamageAmount, struct FDamageEven
 	return ActualDamage;
 }
 
+void ADMC_EnemyCharacterBase::UpdateBufferLogic(float DeltaTime)
+{
+	if (!bIsBuffering || !BufferCurve) return;
+	
+	BufferTimeElapsed += DeltaTime;
+		
+	if (BufferTimeElapsed <= BufferDuration)
+	{
+		float Alpha = BufferCurve->GetFloatValue(BufferTimeElapsed);
+		FVector CurrentLoc = GetActorLocation();
+		FVector Forward = GetActorForwardVector();
+		
+		// The 60.f ensures movement feels the same regardless of frame rate
+		FVector TargetLoc = CurrentLoc + (Forward * CurrentBufferAmount);
+		FVector NewLoc = FMath::Lerp(CurrentLoc, TargetLoc, Alpha * DeltaTime * 60.f);
+			
+		SetActorLocation(NewLoc, true);
+	}
+	else
+	{
+		StopBuffer();
+	}
+}
+
 void ADMC_EnemyCharacterBase::PlayHitReaction(EDMC_DamageType DamageDirection)
 {
-	UAnimMontage* MontageToPlay = nullptr;
-
-	switch (DamageDirection)
+	if (HitReactionMap.Contains(DamageDirection))
 	{
-	case EDMC_DamageType::EDT_Left:
-		StartBuffer(-6.0f);
-		MontageToPlay = HitReactionLeft;
-		break;
-	case EDMC_DamageType::EDT_Right:
-		StartBuffer(-6.0f);
-		MontageToPlay = HitReactionRight;
-		break;
-	case EDMC_DamageType::EDT_Middle:
-		StartBuffer(-6.0f);
-		MontageToPlay = HitReactionMiddle;
-		break;
-	case EDMC_DamageType::EDT_KnockBack:
-		StartBuffer(-10.0f);
-		MontageToPlay = HitReactionKnockBack;
-		break;
-	default:
-		break;
-	}
-	
-	if (MontageToPlay)
-	{
-		PlayAnimMontage(MontageToPlay);
+		const FDMC_HitReactionData& Data = HitReactionMap[DamageDirection];
+		
+		StartBuffer(Data.PushbackAmount);
+		if (Data.HitReactMontage)
+		{
+			PlayAnimMontage(Data.HitReactMontage);
+		}
 	}
 }
 
